e1828e0e159b480acb7ebd1617a0ddf7f989e04f
Ethan Seal <ethan.seal@kearsarge.org>
2024-09-09 12:21:02 -0400

    setup

---
+++ Hello.py
@@ -0,0 +1,58 @@
+name = input("what is your name? ")
+greeting = "Well Howdy!"
+print (greeting + " " + name) 
+game = input("What is your favorite video game? ")
+if game == "Minecraft":
+    print("Minecraft? Thats a basic game!")
+elif game == "minecraft":
+    print("Minecraft? Thats a basic game!")
+elif game == "Roblox":
+    print("Roblox?? I HATE THAT GAME!!")
+elif game == "roblox":
+ print("Roblox?? I HATE THAT GAME!!")
+elif game == "Undertale":
+ print ("That answer fills me with determination.")
+elif game == "undertale":
+ print ("That answer fills me with determination.")
+else:
+ print("I love " + game +  " Aswell!!")
+ 
+food = input ("Games are cool but lets switch the topic! Whats your favorite food! ")
+if food == "Spaghetti":
+    print ("Thats a spicy meataball! " + name + "!")
+elif food == "spaghetti":
+    print ("Thats a spicy meataball! " + name + "!")
+elif food == "Water":
+    print ("Water?? thats not a food! " + name + "!")
+elif food == "water":
+    print ("Water?? Thats not a food! " + name + "!")
+else:
+    print ("Im a hugee fan of " + food + " Also! " + name)
+
+cat = ""
+while cat.lower() != "yes" and cat.lower() != "no":
+    cat = input ("Say.. Are you a fan of cats? ") 
+    if cat == "no":
+        print ("thats a shame.")
+    elif cat == "No":
+        print ("thats a shame.")
+    elif cat == "Yes":
+        print ("Samee meowww :3")
+    elif cat == "yes":
+        print ("Samee meowww :3")
+    else:
+        print ("Its a yes or no question. " + name )
+country = input ("Okay last question which is kind of random.. What's your favorite country! ")
+if country == "America":
+    print ("God bless the land of the free o7")
+elif country == "america": 
+    print ("God bless the land of the free o7")
+elif country == "United States":
+    print ("God Bless AMERICA!!")
+elif country == "Usa":
+    print ("God Bless MERICA and beer!!")
+elif country == "USA": 
+    print ("God Bless MERICA!!")
+elif country == "usa":
+    print ("God Bless MERICA!!")
+
